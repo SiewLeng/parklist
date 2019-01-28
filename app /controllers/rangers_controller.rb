067b6d2deec64b4422edf38e62cbb81700f95c44
SiewLeng <siewleng.lim@gmail.com>
2019-01-28 16:13:00 +0800

    basic

---
+++ app /controllers/rangers_controller.rb
@@ -0,0 +1,40 @@
+class RangersController < ApplicationController
+  def index
+  # test to see if we are at /parks/:id/rangers or /rangers
+    if params.has_key?(:park_id)
+      # get all the rangers for a specific park
+      @park = Park.where(park_id: params[:park_id])
+      @rangers = Ranger.where(park_id: params[:park_id] )
+    else
+      # get all rangers
+      @rangers = Ranger.all
+    end
+  end
+
+  def new
+    @parks = Park.all
+  end
+
+  def create
+    @ranger = Ranger.new(ranger_params)
+
+    @ranger.save
+    redirect_to @ranger
+  end
+
+  def show
+    # deal with the case that we are trying to get a ranger for a park that doesn't exist
+
+    @ranger = Ranger.find(params[:id])
+
+    if params[:park_id].to_i != @ranger.park.id
+      # do something
+    end
+  end
+
+private
+
+  def ranger_params
+    params.require(:ranger).permit(:name, :park_id)
+  end
+end
